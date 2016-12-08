955d3fdb931e7c1d6efea1fd98c23b8679f4e830
Zsolt Zsuffa <zsolt.zsuffa@gmail.com>
2016-12-08 12:02:49 +0100

    successfully deployed to AWS

--- fitnesse-docker-content/docker/build.sh
+++ fitnesse-docker-content/docker/build.sh
@@ -5,4 +5,4 @@ cp -r ../../../FitNesseConnect/fitnesse-connect-connect build
 cp -r ../../../FitNesseConnect/fitnesse-connect-parent build
 cp -r ../../../FitNesseConnect/fitnesse-connect-test build
 docker build -t zsuffazs/fitnesse-docker-content .
-docker run -d --name fitnesse-docker-content -v /fitnesse -p 21:21 -p 30000-30009:30000-30009 -e "PUBLICHOST=localhost" zsuffazs/fitnesse-docker-content
+docker run -d --name fitnesse-docker-content -v /fitnesse zsuffazs/fitnesse-docker-content
